de9430ca95a2b7b6a97cc737009979a4c6c51b9c
Guatagua <sebaiyassin92@gmail.com>
2024-05-28 14:51:23 +0100

    Initial commit

---
+++ ds_app/src/test/java/com/volk_java/ds_app/DsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volk_java.ds_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
